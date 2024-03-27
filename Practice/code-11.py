451272c568f54069bbfbcc589faa483e29498023
MUHAMMAD REHAN AFZAL <149580780+RehanAfzalkhan@users.noreply.github.com>
2024-03-27 22:40:31 +0500

    Delete Practice/code-11.py

--- Practice/code-11.py
+++
@@ -1,9 +0,0 @@
-#iris flower dataset
-#multiclass classification
-# Species (target)
-#features(sepal length,sepal width,petal length,petal width)
-# import numpy as np
-import pandas as pd
-
-from sklearn.neighbors import KNeighborsClassifier
-
